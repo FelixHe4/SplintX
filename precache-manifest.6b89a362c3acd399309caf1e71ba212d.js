f685356fb7eb0513faf2b67e4180275db6280d6e
unknown <forrest.c.fan@gmail.com>
2020-07-15 10:45:59 -0400

    Updates

---
+++ precache-manifest.6b89a362c3acd399309caf1e71ba212d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54940a08a78b44e2cc066d604979bb3a",
+    "url": "/splintx/index.html"
+  },
+  {
+    "revision": "5066ca913109bc50b2fc",
+    "url": "/splintx/static/css/main.d779f3b9.chunk.css"
+  },
+  {
+    "revision": "f7f911cf333931a2b550",
+    "url": "/splintx/static/js/2.a1baf637.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/splintx/static/js/2.a1baf637.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5066ca913109bc50b2fc",
+    "url": "/splintx/static/js/main.5ffc279a.chunk.js"
+  },
+  {
+    "revision": "d8be0d6399afd4b5ad23",
+    "url": "/splintx/static/js/runtime-main.cc7b5e54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/splintx/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dd770b4336c45d2a9c8c58e8fbd7c322",
+    "url": "/splintx/static/media/search.dd770b43.png"
+  }
+]);
